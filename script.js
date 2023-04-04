3434d7c6846040610b3e0cf8f80e4fa661d40763
bkleinb78 <y2bk728@aol.com>
2023-04-04 10:34:00 -0500

    finished updating the script.js functions

--- script.js
+++ script.js
@@ -12,3 +12,29 @@ function randHexColor() {
     }
     return colorHex;
 }
+
+function setGradient() {
+    body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+
+    css.textContent = body.style.background + ";";
+}
+
+function randGradient() {
+    var hex1 = randHexColor();
+    var hex2 = randHexColor();
+
+    body.style.background = "linear-gradient(to right, " + hex1 + ", " + hex2 + ")";
+
+    css.textContent = body.style.background + ";";
+
+    color1.value = hex1;
+    color2.value = hex2;
+}
+
+setGradient();
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
+
+buttonC.addEventListener("click", randGradient);
